ab326c3f0eff9083ff581f616009f6f53b2ea6d5
namdang-fdp <namdangcoder@gmail.com>
2025-06-16 00:10:26 +0700

    feat: add VITE_API_URL interface

---
+++ user-ui/vite-env.d.ts
@@ -0,0 +1,14 @@
+interface ImportMetaEnv {
+    readonly VITE_YOUR_URL: string;
+    readonly VITE_REALM: string;
+    readonly VITE_CLIENT_ID: string;
+   
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
+
+interface ImportMetaEnv {
+    readonly VITE_API_URL: string;
+}
