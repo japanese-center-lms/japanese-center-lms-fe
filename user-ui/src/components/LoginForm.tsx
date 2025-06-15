124ccb37dd58b31ee62c39f07343c0631c22feaa
namdang-fdp <namdangcoder@gmail.com>
2025-06-16 01:11:14 +0700

    feat: add basic login form

---
+++ user-ui/src/components/LoginForm.tsx
@@ -0,0 +1,62 @@
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Loader2 } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import { useLoginForm } from "@/hooks/auth/useLoginForm"
+import { Link } from "react-router-dom";
+
+export default function LoginForm() {
+    const { mutation, form } = useLoginForm();
+
+    return (
+        <Form {...form}>
+            <form
+                onSubmit={form.handleSubmit((values) =>
+                    mutation.mutate(values),
+                )}
+            >
+                <div className="flex flex-col gap-6">
+                    <FormField 
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                            <FormItem className="grid gap-2">
+                                <FormLabel>Email</FormLabel>
+                                <FormControl>
+                                    <Input {...field} required/>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField 
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                            <FormItem className="grid gap-2">
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <Input 
+                                        type="password"
+                                        {...field}
+                                        required
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+            </form>
+        </Form>
+    )
+}
